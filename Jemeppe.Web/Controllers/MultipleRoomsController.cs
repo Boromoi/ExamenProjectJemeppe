e5e3c49f1c5e2cad65a9aa0c05f7a7c0b8cc78fe
LiamSRoc <54795489+LiamSRoc@users.noreply.github.com>
2021-07-02 10:36:01 +0200

    Added Rooms

---
+++ Jemeppe.Web/Controllers/MultipleRoomsController.cs
@@ -0,0 +1,67 @@
+﻿using Jemeppe.Web.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Jemeppe.Web.Controllers
+{
+    public class MultipleRoomsController : Controller
+    {
+        private readonly ILogger<MultipleRoomsController> _logger;
+
+        public MultipleRoomsController(ILogger<MultipleRoomsController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult DeRodeKamer()
+        {
+            return View();
+        }
+
+        public IActionResult DeAvondenKamer()
+        {
+            return View();
+        }
+
+        public IActionResult DeDonkereKamer()
+        {
+            return View();
+        }
+
+        public IActionResult DeDriestuiversKamer()
+        {
+            return View();
+        }
+
+        public IActionResult DeGeheimeKamer()
+        {
+            return View();
+        }
+
+        public IActionResult DeOpperlandseKamer()
+        {
+            return View();
+        }
+
+        public IActionResult DeVersierdeKamer()
+        {
+            return View();
+        }
+
+        public IActionResult DeDerdeKamer()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
